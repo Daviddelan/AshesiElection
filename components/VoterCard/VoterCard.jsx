ae6d80ec875f2720ffc79d2037b1a37fc3d1bfe4
Daviddelan <dela.nuworkpor@gmail.com>
2024-08-09 15:31:19 +0000

    error fix

--- components/VoterCard/VoterCard.jsx
+++ components/VoterCard/VoterCard.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 import Style from '../card/card.module.css'
 import images from '../../assets'
-import voterCardStyle from './voterCard.module.css'
+import voterCardStyle from '/components/voterCard.module.css'
  
 const voterCard = ({voterArray}) => {
 
